3c0b108de1dda4d9070269d86cc9fdddbf770154
Soetji Anto <soetji@gmail.com>
2024-02-26 09:41:41 -0500

    Emit declaration for ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import typescript from '@rollup/plugin-typescript';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,6 +24,7 @@ export default defineConfig({
           react: 'React',
         },
       },
+      plugins: [typescript()]
     },
   },
   plugins: [react()],
